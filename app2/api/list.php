916ef7f8834c907930de124bb615127caf143bd8
githubfollowme <flymayday112@gmail.com>
2021-12-30 11:40:23 +0800

    從後台方式產生網頁內容

---
+++ app2/api/list.php
@@ -0,0 +1,19 @@
+<?php include_once "base.php";
+$sql="SELECT `classroom` as '班級',count
+(`id`) as '人數' FROM `students` GROUP by `classroom`;";
+$rows=$Stu->q($sql);
+
+//使用後端方式產生網頁內容
+//echo json_encode($rows);
+$list="";
+
+foreach($rows as $row){
+    $list.="<div class='card'>";
+    $list.="<div class='card-body'>";
+    $list.="<h5 class='card-title'>{$row['班級']}</h5>";
+    $list.="<p class='card-text'>{$row['人數']}</p>";
+    $list.="</div>";
+    $list.="</div>";
+}
+
+echo $list;
